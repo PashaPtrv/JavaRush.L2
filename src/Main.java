7d74cab22566841b5373b163f8458a3666715563
Pasha <pvlptrv@yandex.ru>
2022-03-02 18:35:48 +0300

    Задания к лекции 2.

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import javax.xml.catalog.Catalog;
+
+public class Main {
+
+
+    public static void main(String[] args){
+        //Создать объект типа Cat 2 раза.
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+    }
+
+    public static class Cat {
+
+    }
+
+
+
+
+}
