0e43f4fe4d3e938242f919e1481f739637c29659
Pasha <pvlptrv@yandex.ru>
2022-03-02 18:55:42 +0300

    Лекция 2, блок заданий 2.

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,36 @@ public class Main {
 
 
     public static void main(String[] args){
-        //Создать объект типа Cat 2 раза.
+   /*     //Создать объект типа Cat 2 раза.
         Cat cat1 = new Cat();
         Cat cat2 = new Cat();
     }
 
-    public static class Cat {
+    public static class Cat {  */
+
+
+        //Создать 3 объекта типа Dog (собака) и присвоить им имена "Max", "Bella", "Jack".
+        Dog dog1 = new Dog();
+        dog1.name = "Max";
+
+        Dog dog2 = new Dog();
+        dog2.name = "Bella";
+
+        Dog dog3 = new Dog();
+
+        dog3.name = "Jack";
+
+        //Напиши программу, которая выводит на экран надпись:
+
+        System.out.println("Мне так плохо! Хочу, чтобы все умерли!");
+
 
     }
 
+    public static class Dog {
 
+        public String name;
 
+    }
 
 }
