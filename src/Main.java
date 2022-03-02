9ccce73f9d14afbfad009423add8d1bfec18f5fd
Pasha <pvlptrv@yandex.ru>
2022-03-02 19:38:45 +0300

    Лекция 2, блок заданий 3.3

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,50 @@ import javax.xml.catalog.Catalog;
 public class Main {
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        //Создайте 10 переменных типа Cat и 8 объектов типа Cat.
 
-        Cat cat1;
-        Cat cat2;
-        Cat cat3 = new Cat();
-        Cat cat4 = new Cat();
-        Cat cat5 = new Cat();
-        Cat cat6 = new Cat();
-        Cat cat7 = new Cat();
-        Cat cat8 = new Cat();
-        Cat cat9 = new Cat();
-        Cat cat10 = new Cat();
+        /* Создайте объект типа Cat(кот), объект типа Dog (собака), объект типа Fish (рыбка) и объект типа Woman.
+            Присвойте каждому животному владельца (owner).*/
+
+        Woman Anna = new Woman();
+
+        Cat cat = new Cat();
+
+        cat.owner = Anna;
+
+        Dog dog = new Dog();
+
+        dog.owner = Anna;
+
+        Fish fish = new Fish();
+
+        fish.owner = Anna;
 
     }
 
     public static class Cat{
+        public Woman owner;
+    }
+    public static class Dog{
+        public Woman owner;
+    }
+    public static class Fish{
+        public Woman owner;
+    }
+    public static class Woman{
 
     }
 
 
+
+
 }
+
+
+
+
+
+
+
 
