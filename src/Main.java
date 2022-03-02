689cf7964c1ee7519d298d261afe4131187e0df6
Pasha <pvlptrv@yandex.ru>
2022-03-02 20:31:36 +0300

    Лекция 2, блок заданий 4. вызов функций.

--- src/Main.java
+++ src/Main.java
@@ -4,42 +4,58 @@ public class Main {
 
 
     public static void main(String[] args) {
+        //Вызываем функции и присваиваем им значения для проверки.
+        System.out.println(min(12,4));
+        System.out.println(max(5,10));
+        System.out.println(minThreeNum(23,12,3));
+        System.out.println(minFourNum(45,34,12,2));
+        printThreeLines("Привет, когда я стану сеньором?");
+        printThreeWords("Скоро");
 
 
-        /* Создайте объект типа Cat(кот), объект типа Dog (собака), объект типа Fish (рыбка) и объект типа Woman.
-            Присвойте каждому животному владельца (owner).*/
-
-        Woman Anna = new Woman();
-
-        Cat cat = new Cat();
-
-        cat.owner = Anna;
-
-        Dog dog = new Dog();
-
-        dog.owner = Anna;
-
-        Fish fish = new Fish();
-
-        fish.owner = Anna;
 
     }
-
-    public static class Cat{
-        public Woman owner;
+    //Написать функцию, которая возвращает минимум из двух чисел.
+    public static int min(int a, int b){
+        if (a > b)
+            return b;
+            else return a;
     }
-    public static class Dog{
-        public Woman owner;
+    //Написать функцию, которая вычисляет максимум из двух чисел.
+    public static int max(int a, int b){
+        if (a > b) return a;
+        else return b;
     }
-    public static class Fish{
-        public Woman owner;
-    }
-    public static class Woman{
+    //Написать функцию, которая вычисляет минимум из трёх чисел.
+    public static int minThreeNum(int a, int b, int c){
+        if (a < b && a < c) {
+            return a;
+        } else if (b < c){
+            return b;
+        }   else return c;
 
     }
-
-
-
+    //	Минимум четырёх чисел ,(вызывать) функцию min(a,b)
+    public static int minFourNum(int a, int b, int c, int d){
+        if (min(a, b) < c && min(a, b) < d){
+            return min(a, b);
+        } else if (c < d) {
+            return c;
+        } else return d;
+    }
+    //Написать функцию, которая выводит переданную строку на экран три раза, каждый раз с новой строки.
+    public static void printThreeLines(String str){
+        for (int i = 0; i < 3; i++){
+            System.out.println(str);
+        }
+    }
+    /*Написать функцию, которая выводит переданную строку (слово) на экран три раза,
+          но в одной строке. Слова должны быть разделены пробелом и не должны сливаться в одно. */
+    public static void printThreeWords(String words){
+        for (int i = 0; i < 3; i++){
+            System.out.print(words + " ");
+        }
+    }
 
 }
 
